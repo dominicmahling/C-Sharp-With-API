ab5757682757c31846f2b9adc3effaf7cc0b5780
d0s1 <dominic1020@web.de>
2026-01-11 15:20:52 +0100

    small cleanup

--- TestApi/Controllers/ProductsController.cs
+++ TestApi/Controllers/ProductsController.cs
@@ -7,13 +7,41 @@ namespace TestApi.Controllers;
 [Route("api/products")]
 public class ProductsController : ControllerBase
 {
+    private static List<Product> _products = new()
+    {
+        new Product { Id = 1, Name = "Laptop" },
+        new Product { Id = 2, Name = "Mouse" }
+    };
+
     [HttpGet]
     public List<Product> GetProducts()
     {
-        return new List<Product>
-        {
-            new Product { Id = 1, Name = "Laptop" },
-            new Product { Id = 2, Name = "Mouse" }
-        };
+        return _products;
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetProductById(int id)
+    {
+        var product =  _products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+    [HttpPost]
+    public ActionResult<Product> CreateProduct(Product product)
+    {
+        product.Id = _products.Max(p => p.Id) + 1;
+        _products.Add(product);
+
+        return CreatedAtAction(
+            nameof(GetProductById),
+            new { id = product.Id },
+            product
+        );
+    }
+
 }
